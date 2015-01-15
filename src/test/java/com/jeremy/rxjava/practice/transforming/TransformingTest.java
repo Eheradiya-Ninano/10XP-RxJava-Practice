3d4d484aceb7e5055b6c1dc773ec11c2d11b2be6
JeremyJeon <JeremyJeon@172.21.22.196>
2015-01-15 15:08:31 +0900

    Transforming 테스트 코드 추가

---
+++ src/test/java/com/jeremy/rxjava/practice/transforming/TransformingTest.java
@@ -0,0 +1,17 @@
+package com.jeremy.rxjava.practice.transforming;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TransformingTest extends TestCase {
+	
+	@Test
+	public void testRunSwitchMapPractice() throws Exception {
+		
+		Transforming.runSwitchMapPractice();
+		
+		assertTrue(true);
+	}
+
+}
